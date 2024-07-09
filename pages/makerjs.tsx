df5ff55a8cfe5f4fbd31a0c2b2b78194d0d73eef
Mark Valentino <mike9377mike9377@gmail.com>
2024-07-09 00:50:13 -0400

    Added sliders

--- pages/makerjs.tsx
+++ pages/makerjs.tsx
@@ -3,33 +3,80 @@ import makerjs from "makerjs";
 
 const Home: FC = () => {
   const [svgData, setSVGData] = useState<string | null>(null);
+  const [width, setWidth] = useState<number>(100);
+  const [height, setHeight] = useState<number>(100);
 
-  const draw = () => {
-    const line = {
-      type: "line",
-      origin: [0, 0],
-      end: [50, 50],
-    };
+  const draw = (width: number, height: number) => {
+    const pathArray = [
+      {
+        type: "line",
+        origin: [0, 0],
+        end: [0, height],
+      },
+      {
+        type: "line",
+        origin: [0, height],
+        end: [width, height],
+      },
+      {
+        type: "line",
+        origin: [width, height],
+        end: [width, 0],
+      },
+      {
+        type: "line",
+        origin: [0, 0],
+        end: [width, 0],
+      },
+    ];
 
-    const circle = {
-      type: "circle",
-      origin: [0, 0],
-      radius: 50,
-    };
-
-    const pathArray = [line, circle];
-
-    const svg = makerjs.exporter.toSVG(pathArray, {scale: 1.1});
+    const svg = makerjs.exporter.toSVG(pathArray, {
+      // scale: 1.2,
+      strokeWidth: "0.5mm",
+    });
     setSVGData(svg);
   };
 
   useEffect(() => {
-    draw();
-  }, []);
+    draw(width, height);
+  }, [width, height]);
+
+  const handleWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWidth(Number(event.target.value));
+  };
+
+  const handleHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHeight(Number(event.target.value));
+  };
 
   return (
     <div>
-      {svgData && <div dangerouslySetInnerHTML={{ __html: svgData }} />}
+      <div>
+        <label>Width: </label>
+        <input
+          type="range"
+          min="10"
+          max="200"
+          value={width}
+          onChange={handleWidthChange}
+        />
+      </div>
+      <div>
+        <label>Height: </label>
+        <input
+          type="range"
+          min="10"
+          max="200"
+          value={height}
+          onChange={handleHeightChange}
+        />
+      </div>
+      {svgData && (
+        <div
+          style={{ margin: 30 }}
+          dangerouslySetInnerHTML={{ __html: svgData }}
+        />
+      )}
     </div>
   );
 };
